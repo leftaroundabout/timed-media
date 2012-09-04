bb735dd3883383085c83317912729bd530708665
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2012-09-04 15:59:46 +0200

    Wrote tester instance of Chunky, to allow debugging of Timeline functions in ghci

---
+++ test/TlChunk.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+import MMedia.Timeline
+import MMedia.Timecode.Arith
+
+
+newtype TlChunkTest a = TlChunkTest [(Timecode,a)]
+                            deriving (Eq, Show)
+
+type TimelineTest a = Timeline () (TlChunkTest a)
+
+δs = oneSecond %/ 30
+
+instance (Show a) => Show (TimelineTest a) where
+  show = (++", ...]") . init . show
+           . take 10 . staticRenderTimeline (oneSecond %/ 10) (timecode(0:0:[0])) ()
+
+
+timelineTest :: a -> TimelineTest a
+timelineTest a = Timeline rt
+ where rt δt t₀ tpre = TimePresentation ( chunkGen $ t₀ @-% nPreload *% δt )
+                                        nPreload
+        where chunkGen tᵢ = TimeRendering $ \()
+                    -> ( TlChunkTest [ (tᵢ @+% δs %* i, a)
+                                      | i<-[0 .. floor $ δt %/% δs -1 ] ]
+                       , chunkGen (tᵢ @+% δt)                         )
+              nPreload = ceiling $ tpre %/% δt
+
+
+instance Chunky (TlChunkTest a) where
+  switchOvr t (TlChunkTest ls) (TlChunkTest ls')
+    = TlChunkTest $ take nLeft ls ++ drop nLeft ls'
+   where nLeft = floor $ t %/% δs
+
+timeChainTest :: [(Double, a)] -> TimelineTest a
+timeChainTest = timeChain . map(\(tc,n)->(timecode[tc],timelineTest n))
