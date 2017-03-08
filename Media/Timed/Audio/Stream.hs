40c0592d3c57fd43a4791844d1d8217cca7a345a
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-03-08 01:08:44 +0100

    Very basic proof of concept.

--- Media/Timed/Audio/Stream.hs
+++ Media/Timed/Audio/Stream.hs
@@ -13,11 +13,22 @@ module Media.Timed.Audio.Stream where
 
 
 import Math.FunctionalAnalysis.L2Function.R1
+import qualified Data.Vector.Generic as Arr
 
+import qualified Sound.Pulse.Simple as PA
 
 
 data Audio = Audio {
-    audioStreamLengthExponent :: Int  -- ^ 2**time in seconds
+    audioStreamLengthExponent :: Int  -- ^ Duration in seconds = 2**lengthExponent
   , audioSignal :: UnitL2 Int Double
   }
 
+
+playMonoAudio :: Audio -> IO ()
+playMonoAudio (Audio ldt signal) = do
+    s <- PA.simpleNew Nothing "example" PA.Play Nothing "Sobolev audio signal"
+          (PA.SampleSpec (PA.F32 PA.LittleEndian) 44100 1) Nothing Nothing
+    PA.simpleWrite s . Arr.toList $ Arr.map (realToFrac::Double->Float) uniformSpldSig
+    PA.simpleDrain s
+    PA.simpleFree s
+ where uniformSpldSig = toUniformSampled (round $ 44100 * 2^^ldt) signal
