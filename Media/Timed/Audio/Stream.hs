3664bbe46126125187f66aa9910e2cc0ab1ea3a4
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-03-09 00:42:33 +0100

    Basic audio loading with hsndfile.
    Performance and memory usage are abysmal, and quality seems not quite
    ok either (problems of the `sobolev-space` types), but... it works, with
    actual music from e.g. Ogg Vorbis files!

--- Media/Timed/Audio/Stream.hs
+++ Media/Timed/Audio/Stream.hs
@@ -8,6 +8,7 @@
 -- Portability : portable
 -- 
 -- 
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Media.Timed.Audio.Stream where
 
@@ -16,6 +17,12 @@ import Math.FunctionalAnalysis.L2Function.R1
 import qualified Data.Vector.Generic as Arr
 
 import qualified Sound.Pulse.Simple as PA
+
+import qualified Sound.File.Sndfile as SF
+import qualified Sound.File.Sndfile.Buffer.Vector as BV
+
+import Data.Default
+import Data.Monoid
 
 
 data Audio = Audio {
@@ -32,3 +39,19 @@ playMonoAudio (Audio ldt signal) = do
     PA.simpleDrain s
     PA.simpleFree s
  where uniformSpldSig = toUniformSampled (round $ 44100 * 2^^ldt) signal
+
+
+loadAudio :: FilePath -> IO [Audio]
+loadAudio fPath = do
+    (SF.Info nFr sr nChan _ _ _, Just (buf :: BV.Buffer Double)) <- SF.readFile fPath
+    let bufv = BV.fromBuffer buf
+        nSpl = Arr.length bufv
+        len | nSpl == nFr * nChan
+          = fromIntegral nFr / fromIntegral sr
+        ldlAudio = ceiling $ logBase 2 len
+        nTot = sr * 2^ldlAudio
+        silence = Arr.replicate (nTot-nFr) 0
+    return [ Audio ldlAudio . fromUniformSampled def $ chanv <> silence
+           | i <- [0..nChan-1]
+           , let chanv = Arr.generate nFr $ \j -> bufv Arr.! (j*nChan + i)
+           ]
